9a866dde87d5cfc98cd886a2c8e33f145100acf2
Piyush Verma <github@piyushverma.net>
2014-10-29 21:03:49 +0530

    - replaced with _ since querying becomes tougher that way

--- src/cdnlysis/cloudfront.go
+++ src/cdnlysis/cloudfront.go
@@ -13,45 +13,45 @@ const TAB = "\t"
 var COLUMNS = []string{
 	"date",
 	"time",
-	"x-edge-location",
-	"sc-bytes",
-	"c-ip",
-	"cs-method",
-	"cs(Host)",
-	"cs-uri-stem",
-	"sc-status",
-	"cs(Referer)",
-	"cs(User-Agent)",
-	"cs-uri-query",
-	"cs(Cookie)",
-	"x-edge-result-type",
-	"x-edge-request-id",
-	"x-host-header",
-	"cs-protocol",
-	"cs-bytes",
-	"time-taken",
+	"x_edge_location",
+	"sc_bytes",
+	"c_ip",
+	"cs_method",
+	"cs_host",
+	"cs_uri_stem",
+	"sc_status",
+	"cs_referer",
+	"cs_user_agent",
+	"cs_uri_query",
+	"cs_cookie",
+	"x_edge_result_type",
+	"x_edge_request_id",
+	"x_host_header",
+	"cs_protocol",
+	"cs_bytes",
+	"time_taken",
 }
 
 type LogEntry struct {
 	Date           string  `json:"date"`
 	Time           string  `json:"time"`
-	EdgeLocation   string  `json:"x-edge-location"`
-	BytesSent      int64   `json:"sc-bytes,string"`
-	IP             string  `json:"c-ip"`
-	Method         string  `json:"cs-method"`
-	Host           string  `json:"cs(Host)"`
-	UriStem        string  `json:"cs-uri-stem"`
-	Status         int64   `json:"sc-status,string"`
-	Referer        string  `json:"cs(Referer)"`
-	UserAgent      string  `json:"cs(User-Agent)"`
-	UriQuery       string  `json:"cs-uri-query"`
-	Cookie         string  `json:"cs(Cookie)"`
-	EdgeResultType string  `json:"x-edge-result-type"`
-	EdgeRequestId  string  `json:"x-edge-request-id"`
-	HostHeader     string  `json:"x-host-header"`
-	Protocol       string  `json:"cs-protocol"`
-	BytesReceived  int64   `json:"cs-bytes,string"`
-	TimeTaken      float64 `json:"time-taken,string"`
+	EdgeLocation   string  `json:"x_edge_location"`
+	BytesSent      int64   `json:"sc_bytes,string"`
+	IP             string  `json:"c_ip"`
+	Method         string  `json:"cs_method"`
+	Host           string  `json:"cs_host"`
+	UriStem        string  `json:"cs_uri_stem"`
+	Status         int64   `json:"sc_status,string"`
+	Referer        string  `json:"cs_referer"`
+	UserAgent      string  `json:"cs_user_agent"`
+	UriQuery       string  `json:"cs_uri_query"`
+	Cookie         string  `json:"cs_cookie"`
+	EdgeResultType string  `json:"x_edge_result_type"`
+	EdgeRequestId  string  `json:"x_edge_request_id"`
+	HostHeader     string  `json:"x_host_header"`
+	Protocol       string  `json:"cs_protocol"`
+	BytesReceived  int64   `json:"cs_bytes,string"`
+	TimeTaken      float64 `json:"time_taken,string"`
 }
 
 func ParseRecord(log_record string) []interface{} {
@@ -66,17 +66,15 @@ func ParseRecord(log_record string) []interface{} {
 	datetime := ""
 
 	for ix, item := range COLUMNS {
-		log.Println(item)
-
 		val := split[ix]
 
 		if item == "date" {
 			datetime += val
 		}
 
-		if item == "cs(Referer)" ||
-			item == "cs(User-Agent)" ||
-			item == "cs-uri-query" {
+		if item == "cs_referer" ||
+			item == "cs_user_agent" ||
+			item == "cs_uri_query" {
 			unescaped, err := url.QueryUnescape(val)
 			if err != nil {
 				log.Println(err)
@@ -86,13 +84,13 @@ func ParseRecord(log_record string) []interface{} {
 			continue
 		}
 
-		if item == "sc-bytes" ||
-			item == "sc-status" ||
-			item == "cs-bytes" {
+		if item == "sc_bytes" ||
+			item == "sc_status" ||
+			item == "cs_bytes" {
 			conv, _ := strconv.ParseInt(val, 10, 64)
 			record = append(record, conv)
 
-		} else if item == "time-taken" {
+		} else if item == "time_taken" {
 			conv, _ := strconv.ParseFloat(val, 64)
 			record = append(record, conv)
 
